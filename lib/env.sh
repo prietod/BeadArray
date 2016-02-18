1dd78157a2b64fb5b89e2a0c7c206aa541a27bc6
Kevin Counts <counts@digicat.org>
2016-02-18 18:14:12 -0500

    minor fix of logic for the merge of replaced_samples.txt with static/exclude-data-raw.txt

--- lib/env.sh
+++ lib/env.sh
@@ -6,12 +6,14 @@ setvar() { var="$1"; shift; value="$@"; eval $var=\"${value}\"; }
 # Config
 #-------------------------------------------------------------------------
 
+setvar WORK_DIR                       "$BASE/tmp/work"
+
 setvar JINFINITI_DATA_DIR             "/hiidata/teddy/data/jinfiniti/gene_expression"
 setvar RAW_DATA_DIR                   "$BASE/tmp/data/raw"
 setvar FILTERED_DATA_DIR              "$BASE/tmp/data/filtered"
-setvar RAW_CHIP_LIST                  "$BASE/tmp/work/common/chip-list-raw.txt"
-setvar FILTERED_CHIP_LIST             "$BASE/tmp/work/common/chip-list-filtered.txt"
-setvar RAW_DATA_EXCLUDE_PATTERNS      "$BASE/static/exclude-data-raw.txt"
+setvar RAW_CHIP_LIST                  "${WORK_DIR}/common/chip-list-raw.txt"
+setvar FILTERED_CHIP_LIST             "${WORK_DIR}/common/chip-list-filtered.txt"
+setvar RAW_DATA_EXCLUDE_PATTERNS      "${WORK_DIR}/common/exclude-data-raw.txt"
 
 setvar EXCLUDE_ALL_PATTERN_FILE       "$BASE/tmp/work/common/exclude-all.txt"
 setvar EXCLUDE_NON_DONOR_PATTERN_FILE "$BASE/tmp/work/common/exclude-non-donor.txt"
@@ -40,7 +42,6 @@ setvar STATE_DIR                      "$BASE/tmp/state"
 setvar R_VERSION                      "3.2.3"
 setvar TEST_MODE                      "${TEST_MODE:-false}"
 setvar LOG_DIR                        "$BASE/tmp/log"
-setvar WORK_DIR                       "$BASE/tmp/work"
 
 setvar R_LIBS                         "${HOME}/lib/R/${R_VERSION}"
 
